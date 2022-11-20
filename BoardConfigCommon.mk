45734931fbee4a222293a3512e464abd5856d208
Spartaco Amadei <spamadei@gmail.com>
2022-11-20 03:31:42 +0100

    little reformatting

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,9 +78,10 @@ TARGET_KEYMASTER_VARIANT := samsung
 
 ## Manifest
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE :=  \
     $(COMMON_PATH)/device_framework_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
+
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
